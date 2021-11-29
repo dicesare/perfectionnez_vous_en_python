7bb99c35640e5210c265f6a2b1cce1c299814eec
dicesare <antony.coco@live.fr>
2021-11-29 17:28:39 +0100

    organiser un projet en module

---
+++ analysis/csv.py
@@ -0,0 +1,23 @@
+from os import path
+
+"""
+open() -->  https://docs.python.org/3/library/functions.html#open
+"""
+
+
+def main():
+    pass
+
+
+def launch_analysis(data_file):
+    directory = path.dirname(__file__)
+    path_to_file = path.join(directory, "../data", data_file)
+
+    with open(path_to_file, "r") as f:
+        preview = f.readline()
+
+    print(f"afficher previsualisation du fichier csv : {preview}")
+
+
+if __name__ == '__main__':
+    launch_analysis('current_mps.csv')
---
+++ analysis/xml.py
@@ -0,0 +1,15 @@
+from os import path
+
+
+def launch_analysis(data_file):
+    directory = path.dirname(__file__)
+    path_to_file = path.join(directory, "../data", data_file)
+
+    with open(path_to_file, "r") as f:
+        preview = f.readline()
+
+    print(f"afficher previsualisation du fichier xml : {preview}")
+
+
+if __name__ == "__main__":
+    launch_analysis("SyceronBrut.xml")
