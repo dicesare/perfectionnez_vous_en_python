599ff3149f1b9b93a6247410cee5cecc75a687e5
dicesare <antony.coco@live.fr>
2021-11-29 19:00:28 +0100

    gerer les erreurs les bogs

--- analysis/csv.py
+++ analysis/csv.py
@@ -1,22 +1,19 @@
 from os import path
-
 """
 open() -->  https://docs.python.org/3/library/functions.html#open
 """
 
 
-def main():
-    pass
-
-
 def launch_analysis(data_file):
     directory = path.dirname(__file__)
     path_to_file = path.join(directory, "../data", data_file)
-
     with open(path_to_file, "r") as file:
         preview = file.readline()
+        print(f"afficher previsualisation du fichier xml : {preview}")
 
-    print(f"afficher previsualisation du fichier csv : {preview}")
+
+def main():
+    pass
 
 
 if __name__ == '__main__':
--- analysis/xml.py
+++ analysis/xml.py
@@ -1,14 +1,20 @@
 from os import path
 
+"""
+open() -->  https://docs.python.org/3/library/functions.html#open
+"""
+
 
 def launch_analysis(data_file):
     directory = path.dirname(__file__)
     path_to_file = path.join(directory, "../data", data_file)
-
     with open(path_to_file, "r") as file:
         preview = file.readline()
+        print(f"afficher previsualisation du fichier xml : {preview}")
 
-    print(f"afficher previsualisation du fichier xml : {preview}")
+
+def main():
+    pass
 
 
 if __name__ == "__main__":
